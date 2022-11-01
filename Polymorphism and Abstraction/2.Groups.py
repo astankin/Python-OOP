f9bf41b5af01d6c415b31eac7059f8457125b3a0
astankin <a_stankin@abv.bg>
2022-11-01 20:41:52 +0200

    Adding Class and Static Methods Exercise

---
+++ Polymorphism and Abstraction/2.Groups.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+class Group:
+    def __init__(self, name, people):
+        self.name = name
+        self.people = people
