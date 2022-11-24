3ecfb1d029ffbb64d08de46d1234caffaadecb21
astankin <a_stankin@abv.bg>
2022-11-24 22:25:19 +0200

    Uploading solution Python OOP Exam - Movie App

--- ExamPreparation/Movie/project/movie_app.py
+++ ExamPreparation/Movie/project/movie_app.py
@@ -7,8 +7,16 @@ class MovieApp:
         self.movies_collection = []
         self.users_collection = []
 
+    def check_user_in_collection(self, username):
+        return username in [user.username for user in self.users_collection]
+
     def register_user(self, username: str, age: int):
-        pass
+        if self.check_user_in_collection(username):
+            raise Exception("User already exists!")
+        new_user = User(username, age)
+        self.users_collection.append(new_user)
+        return f"{username} registered successfully."
+
 
 
 
